a23e013d426bdf02705e0fd3575837a6750c5546
FebriJuandaSitepu <febrijuanda250204@gmail.com>
2025-06-26 01:20:01 +0700

    perbaikan

--- database/migrations/2025_06_20_112338_create_pembayaran_table.php
+++ database/migrations/2025_06_20_112338_create_pembayaran_table.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up(): void {
         // 2025_06_24_000002_create_pembayarans_table.php
 
-Schema::create('pembayarans', function (Blueprint $table) {
+Schema::create('pembayaran', function (Blueprint $table) {
     $table->id();
     $table->string('user_id'); // berisi no_identitas konsumen
     $table->string('tipe'); // pemasukan/pengeluaran
